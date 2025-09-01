ccf72aa26c05c9042558a1aa5490f5a01032cfa0
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-01 20:58:11 +0530

    Add custom JPQL query

--- src/main/java/com/myorganisation/gcafe/repository/DishRepository.java
+++ src/main/java/com/myorganisation/gcafe/repository/DishRepository.java
@@ -1,7 +1,10 @@
 package com.myorganisation.gcafe.repository;
 
+import com.myorganisation.gcafe.enums.DishCategory;
 import com.myorganisation.gcafe.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,10 @@ public interface DishRepository extends JpaRepository<Dish, Long> {
     List<Dish> findByNameContaining(String name);
 
     List<Dish> findByNameContainingAndPriceBetween(String q, Double min, Double max);
+
+    List<Dish> findByNameContainingAndPriceBetweenAndCategory(String q, Double min, Double max, DishCategory category);
+
+    //Custom JPQL (Java Persistence Query Language)
+    @Query("SELECT d FROM Dish d WHERE d.id = :identity")
+    Dish getDishById(@Param("identity") Long id);
 }
