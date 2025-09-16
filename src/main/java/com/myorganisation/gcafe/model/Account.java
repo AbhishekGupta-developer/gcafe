f196bdc16e05bd6471723ae29e6faa62b9f2c141
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-16 21:02:01 +0530

    Add Account entity

---
+++ src/main/java/com/myorganisation/gcafe/model/Account.java
@@ -0,0 +1,24 @@
+package com.myorganisation.gcafe.model;
+
+import com.myorganisation.gcafe.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "accounts")
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount = 0D;
+    private Boolean isPaid = true;
+
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status = AccountStatus.ACTIVE;
+
+    @OneToOne
+    @JoinColumn(name = "chef")
+    private Chef chef;
+}
