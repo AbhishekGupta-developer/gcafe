8d39cba5db09c2b53a818e473eb52a3603a983e4
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-22 20:46:38 +0530

    Create getChefPage API

--- src/main/java/com/myorganisation/gcafe/controller/ChefController.java
+++ src/main/java/com/myorganisation/gcafe/controller/ChefController.java
@@ -5,6 +5,7 @@ import com.myorganisation.gcafe.dto.response.ChefResponseDto;
 import com.myorganisation.gcafe.dto.response.GenericResponseDto;
 import com.myorganisation.gcafe.service.ChefService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,5 +42,15 @@ public class ChefController {
     @DeleteMapping
     public ResponseEntity<GenericResponseDto> removeChef(@RequestParam Long id) {
         return new ResponseEntity<>(chefService.removeChef(id), HttpStatusCode.valueOf(200));
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Page<ChefResponseDto>> getChefPage(
+            @RequestParam(defaultValue = "0") Integer pageIndex,
+            @RequestParam(defaultValue = "10") Integer pageSize,
+            @RequestParam(defaultValue = "id") String sortByAttribute,
+            @RequestParam(defaultValue = "asc") String sortInOrder
+    ) {
+        return new ResponseEntity<>(chefService.getChefPage(pageIndex, pageSize, sortByAttribute, sortInOrder), HttpStatusCode.valueOf(200));
     }
 }
