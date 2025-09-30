03d17ca192732f3e6a229e22a72afe163615ac8b
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-09-30 21:25:19 +0530

    Add sample CRON job

---
+++ src/main/java/com/myorganisation/gcafe/cron/CronJobs.java
@@ -0,0 +1,18 @@
+package com.myorganisation.gcafe.cron;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CronJobs {
+
+    int count = 0;
+
+    @Scheduled(cron = "* * 17 * * ?")
+    private void randomJob() {
+        System.out.println("Hello: " + ++count);
+        if(count == 60) {
+            count = 0;
+        }
+    }
+}
