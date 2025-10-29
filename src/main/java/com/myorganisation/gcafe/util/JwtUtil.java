9fecb41320ac36d0b20e0f0e7cd8179fcce8e6df
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-10-29 17:45:50 +0530

    Add JWT Util

---
+++ src/main/java/com/myorganisation/gcafe/util/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.myorganisation.gcafe.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET;
+    private final SecretKey KEY;
+    private final long AUTH_EXPIRATION;
+
+    public JwtUtil(@Value("${jwt.secret}") String jwtSecret) {
+        SECRET = jwtSecret;
+        KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+        AUTH_EXPIRATION = 1000 * 60 * 5; // Valid for 5 mins
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + AUTH_EXPIRATION))
+                .signWith(KEY, Jwts.SIG.HS256)
+                .compact();
+    }
+
+}
