5ffd276ee0ae09cfbff48bc5984783202c644cf4
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 17:59:14 +0530

    Add Email service

---
+++ src/main/java/com/myorganisation/gcafe/service/EmailService.java
@@ -0,0 +1,11 @@
+package com.myorganisation.gcafe.service;
+
+import com.myorganisation.gcafe.dto.request.EmailOtpVerificationRequestDto;
+import com.myorganisation.gcafe.dto.request.EmailRequestDto;
+import com.myorganisation.gcafe.dto.response.GenericResponseDto;
+import com.myorganisation.gcafe.enums.OtpPurpose;
+
+public interface EmailService {
+    GenericResponseDto sendOtp(EmailRequestDto emailRequestDto, OtpPurpose otpPurpose);
+    GenericResponseDto verifyOtp(EmailOtpVerificationRequestDto emailOtpVerificationRequestDto, OtpPurpose purpose);
+}
