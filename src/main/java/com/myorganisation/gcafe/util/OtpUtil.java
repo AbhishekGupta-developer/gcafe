561db47a302c82ae1adbbbf666f7de5ac7711d96
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 19:58:17 +0530

    Add OTP util

---
+++ src/main/java/com/myorganisation/gcafe/util/OtpUtil.java
@@ -0,0 +1,18 @@
+package com.myorganisation.gcafe.util;
+
+import java.security.SecureRandom;
+
+public class OtpUtil {
+    private static final SecureRandom random = new SecureRandom();
+    private static final int OTP_LENGTH = 6;
+
+    public static String generateOtp() {
+        StringBuilder sb = new StringBuilder(OTP_LENGTH);
+        for(int i = 0; i < OTP_LENGTH; i++) {
+            sb.append(random.nextInt(10)); // 0-9 digits
+        }
+
+        return sb.toString();
+    }
+
+}
