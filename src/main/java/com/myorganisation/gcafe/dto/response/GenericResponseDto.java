3d651cef5b7c886005687706d2a9b8bfbea1ce02
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 20:01:21 +0530

    Add Builder annotation and constructor annotation

--- src/main/java/com/myorganisation/gcafe/dto/response/GenericResponseDto.java
+++ src/main/java/com/myorganisation/gcafe/dto/response/GenericResponseDto.java
@@ -1,8 +1,14 @@
 package com.myorganisation.gcafe.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GenericResponseDto {
     private boolean success;
     private String message;
