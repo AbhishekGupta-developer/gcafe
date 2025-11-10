023c6b952f40ffcc1c24b197a44736a00517a418
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 20:08:58 +0530

    Add OtpStore

---
+++ src/main/java/com/myorganisation/gcafe/store/OtpStore.java
@@ -0,0 +1,20 @@
+package com.myorganisation.gcafe.store;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OtpStore {
+    private static final ConcurrentHashMap<String, String> otpMap = new ConcurrentHashMap<>();
+
+    public static void storeOtp(String email, String otp) {
+        otpMap.put(email, otp);
+    }
+
+    public static String getOtp(String email) {
+        return otpMap.get(email);
+    }
+
+    public static void clearOtp(String email) {
+        otpMap.remove(email);
+    }
+
+}
