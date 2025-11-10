182b2b7f61be29f10895d36bfb136cdcc67be3fc
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 20:47:57 +0530

    Refactor SecurityConfig and auth API endpoint (permit all)

--- src/main/java/com/myorganisation/gcafe/config/SecurityConfig.java
+++ src/main/java/com/myorganisation/gcafe/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.myorganisation.gcafe.config;
 
+import com.myorganisation.gcafe.enums.UserRole;
 import com.myorganisation.gcafe.filter.JwtAuthFilter;
+import com.myorganisation.gcafe.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,38 +11,41 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
     private JwtAuthFilter jwtAuthFilter;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 // Disable CSRF (Cross Site Request Forgery) to call ALL HTTP METHODS (GET, POST, PUT, PATCH, DELETE) without domain
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/", "/test").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/dish", "/api/dish/**").permitAll()
-                                .requestMatchers(HttpMethod.DELETE, "/api/dish", "/api/dish/**").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
-                                .requestMatchers("/api", "/api/**").authenticated()
+                                .requestMatchers(HttpMethod.GET, "/").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api").hasRole(UserRole.ADMIN.name())
                                 .anyRequest().authenticated()
                 );
 
 //        httpSecurity.httpBasic(withDefaults());
+
+        httpSecurity.authenticationManager(authenticationManager());
 
         httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
@@ -53,10 +58,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+    public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 
         return new ProviderManager(daoAuthenticationProvider);
     }
