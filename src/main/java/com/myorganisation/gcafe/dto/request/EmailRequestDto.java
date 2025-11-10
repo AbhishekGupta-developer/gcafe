b096f8943aca7b957119e13ed697d15b8f5139d9
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 17:56:05 +0530

    Add Email request DTO

---
+++ src/main/java/com/myorganisation/gcafe/dto/request/EmailRequestDto.java
@@ -0,0 +1,16 @@
+package com.myorganisation.gcafe.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailRequestDto {
+
+    @Email
+    @NotBlank
+    private String email;
+
+}
