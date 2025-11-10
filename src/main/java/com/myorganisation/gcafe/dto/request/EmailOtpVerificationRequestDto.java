c7929fa527d4e4de858faf4439768b7d935c486d
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-10 19:30:42 +0530

    Add Email and OTP verification request DTO

---
+++ src/main/java/com/myorganisation/gcafe/dto/request/EmailOtpVerificationRequestDto.java
@@ -0,0 +1,19 @@
+package com.myorganisation.gcafe.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailOtpVerificationRequestDto {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String otp;
+
+}
