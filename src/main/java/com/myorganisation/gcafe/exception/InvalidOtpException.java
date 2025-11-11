d809326e3c46481489837faeeded40b897b430bf
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-11 20:33:08 +0530

    Add InvalidOtpException

---
+++ src/main/java/com/myorganisation/gcafe/exception/InvalidOtpException.java
@@ -0,0 +1,11 @@
+package com.myorganisation.gcafe.exception;
+
+public class InvalidOtpException extends RuntimeException {
+    public InvalidOtpException() {
+        super("OTP verification failed");
+    }
+
+    public InvalidOtpException(String m) {
+        super(m);
+    }
+}
