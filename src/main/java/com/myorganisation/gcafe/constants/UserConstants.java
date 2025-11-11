303bf09f1fe3d3bdb5c7df5d665d45a8f1982a41
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-11-11 20:32:25 +0530

    Add UserConstant

---
+++ src/main/java/com/myorganisation/gcafe/constants/UserConstants.java
@@ -0,0 +1,9 @@
+package com.myorganisation.gcafe.constants;
+
+public class UserConstants {
+    private UserConstants() {
+
+    }
+
+    public static final String PASSWORD_NOT_SET = "NOT_SET";
+}
